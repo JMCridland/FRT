8b7de4fa58220a05d11e821ef6d5430b0701afd9
JMCridland <juliemcridland@gmail.com>
2023-06-08 11:18:39 -0700

    Add files via upload
    This file identifies the gene or genes associated with each distinguishing SNP from step 1.

---
+++ Identify_genes_for_each_site.pl
@@ -0,0 +1,51 @@
+#!perl -w
+
+use strict;
+
+my $pos = shift(@ARGV) or die;#/data/julie/FemaleRT/ALL_SNPs_table
+my $exon = shift(@ARGV) or die; #/data/FlyRef/Drosophila_melanogaster/6.41/fasta/Dmel_exon_ranges 
+my $output = shift(@ARGV) or die;
+unlink(qq{$output});
+
+my %names = ();
+my %pos = ();
+my %exon = ();
+
+open(P, "<$pos");
+while(my $line = <P>){
+    chomp $line;
+    my @p = split(/\t/, $line);
+    $pos{$p[0] . "\t" . $p[1]} = 1;
+}
+close P;
+
+my %used = ();
+open(E, "<$exon");
+while(my $line = <E>){
+    chomp $line;
+    my @e = split(/\t/, $line);
+    ##FBgn0031081     X       19965197        19965511
+    for(my $i = $e[2]; $i < $e[3]; $i++){
+	if(exists($pos{$e[1] . "\t" . $i})){
+	    if(!(exists($used{$e[1] . "\t" . $i . "\t" . $e[0]}))){
+		push(@{$names{$e[1] . "\t" . $i}}, $e[0]);
+		$used{$e[1] . "\t" . $i . "\t" . $e[0]} = 1;
+	    }
+	}
+    }
+}
+close E;
+
+open(B, ">>$output");
+while((my $k, my $v) = each(%pos)){
+
+    if(!(exists($names{$k}))){
+	print B $k, "\t", "None\n";
+    }elsif(exists($names{$k})){
+	print B $k, "\t", join(",", @{$names{$k}}), "\n";
+    }
+}
+close B;
+
+
+
